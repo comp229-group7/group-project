06abb51a1187bb343e00c09d06d6df301b65c525
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2025-10-18 21:20:12 +0100

    changed db to a working one

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "URI": "mongodb+srv://raphael:Anh82xE5hCsztZEN@midterm-server.dnabr.mongodb.net/midterm?retryWrites=true&w=majority",
+  "URI": "mongodb+srv://andremscruz10_db_user:ouOt1B95a17KrHYM@cluster0.9zfmf3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   "Secret": 'SomeSecret'
 };
